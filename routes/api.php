380364299bae6b58b0bd4b722b57b8e4988e9889
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2023-12-22 22:22:01 -0300

    Iniciando Api

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/ping', function () {
+    return 'pong';
+});
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
