870f2b6a54d4ffb1f7538a989efe5b8273e6c05d
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2024-08-05 22:11:39 -0300

    ajuste de rota

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/ping', function () {
     return 'pong';
 });
+Route::get('/ping', function () {
+    return 'pong';
+});Route::get('/ping', function () {
+    return 'pong';
+});
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
